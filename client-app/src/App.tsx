8a8909b5e44223839fd059b9f617319311e13b92
ilijarushkovski <ilijarushkovski@gmail.com>
2020-08-09 01:08:25 +0200

    End of section3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import './CarItem';
+import axios from 'axios';
+import {Header, Icon, List} from 'semantic-ui-react'
 
-import { cars } from './demo';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    <ul>
-      {cars.map ((car) => (
+  state = {
+    values:[]
+  }
 
-        <CarItem c/>
-      ))}
-    </ul>
-    </div>
-  );
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      this.setState({
+        values: response.data
+      })
+    })
+
+  }
+  render(){
+    return (
+  <div>
+  <Header as='h2'>
+    <Icon name='users' />
+    <Header.Content>Uptime Guarantee</Header.Content>
+  </Header>
+      <List>
+        { this.state.values.map((value: any) => (
+          <List.Item key ={value.id}>
+            {value.name}
+          </List.Item>
+        ))}
+      </List>
+   </div>
+
+
+    );
+  }
 }
 
 export default App;
